02514a360e8577330386b7fcff9543d587d3f9e5
Ali Madooei <1509757+madooei@users.noreply.github.com>
2019-04-02 22:41:56 -0700

    Update isPalindrome.py

--- python-project/isPalindrome.py
+++ python-project/isPalindrome.py
@@ -1,5 +1,4 @@
 # REQUIRES: word is a string
-# EFFECTS: returns true if word is a plindrome
-#    returns false otherwise
+# EFFECTS: returns true if word is a plindrome (false, otherwise)
 def isPalindrome(word):
     return False # stub
